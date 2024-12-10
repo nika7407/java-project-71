6d798a24e7c06f76017e197cc604f0c4adfd1f34
nika7407 <nikala7407@gmail.com>
2024-12-10 16:41:29 +0400

    1.0 first application and dist test

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
