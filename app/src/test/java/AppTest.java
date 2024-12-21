cb07daa71bede2bd7fe0923689383fcf72f4ecd2
nika7407 <nikala7407@gmail.com>
2024-12-21 22:00:04 +0400

    7 trying to make actions work

--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -66,14 +66,14 @@ public class AppTest {
     @Test
     public void testGenerateDiff() {
         String actual = App.generateDiff(fixedMap, fixedMap1);
-        String expected = "{\n" +
-                " - follow: false\n" +
-                "   host: hexlet.io\n" +
-                " - proxy: 123.234.53.22\n" +
-                " - timeout: 50\n" +
-                " + timeout: 20\n" +
-                " + verbose: true\n" +
-                "}";
+        String expected = "{\n"
+               + " - follow: false\n"
+               + "   host: hexlet.io\n"
+               + " - proxy: 123.234.53.22\n"
+               + " - timeout: 50\n"
+               + " + timeout: 20\n"
+               + " + verbose: true\n"
+               + "}";
         assertEquals(expected, actual);
     }
 }
