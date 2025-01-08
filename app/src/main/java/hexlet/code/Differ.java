d7afc941bead41bd32717508f68b0535c5e5197d
nika7407 <nikala7407@gmail.com>
2025-01-08 18:34:41 +0400

    11 checkstyle

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -65,27 +65,6 @@ public class Differ {
         return diff.toString();
     }
 
-    public static String json(Map<String, Object> input1, Map<String, Object> input2){
-        Set<String> allKeys = new TreeSet<>();
-        allKeys.addAll(input1.keySet());
-        allKeys.addAll(input2.keySet());
-
-        StringBuilder diff = new StringBuilder("\n");
-        for (String key : allKeys) {
-            var value1 = input1.get(key);
-            var value2 = input2.get(key);
-
-            if (!input1.containsKey(key)) {
-                diff.append("Property '" + key + "' was added with value: ").append(typeCheck(value2)).append("\n");
-            } else if (!input2.containsKey(key)) {
-                diff.append("Property '" + key + "' was removed").append("\n");
-            } else if (!Objects.deepEquals(value1, value2)) {
-                diff.append("Property '" + key + "' was updated. ")
-                        .append("From " + typeCheck(value1) + " to " + typeCheck(value2)).append("\n");
-            }
-        }
-        return diff.toString();
-    }
 
     public static String typeCheck(Object input) {
         if (input instanceof String) {
