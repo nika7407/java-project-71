0e7f777a583e60d4194439874ff2c7ea16a3c662
nika7407 <nikala7407@gmail.com>
2025-01-09 17:57:38 +0400

    11 added json

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,19 +1,21 @@
 package hexlet.code;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.Objects;
-import java.util.List;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.*;
 
 public class Differ {
 
-    public static String generate(Map<String, Object> input1, Map<String, Object> input2, String format) {
+    public static String generate(Map<String, Object> input1, Map<String, Object> input2, String format) throws JsonProcessingException {
         String answer = "";
         if (format.equals("stylish")) {
             answer = stylish(input1, input2);
         } else if (format.equals("plain")) {
             answer = plain(input1, input2);
+        } else if (format.equals("json"));
+        {
+            answer = json(input1, input2);
         }
         return answer;
     }
@@ -65,6 +67,51 @@ public class Differ {
         return diff.toString();
     }
 
+    public static String json(Map<String, Object> input1, Map<String, Object> input2) throws JsonProcessingException {
+        Set<String> allKeys = new TreeSet<>();
+        allKeys.addAll(input1.keySet());
+        allKeys.addAll(input2.keySet());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        List<Map<String, Object>> answer = new ArrayList<>();
+
+        StringBuilder diff = new StringBuilder("\n");
+        for (String key : allKeys) {
+            var value1 = input1.get(key);
+            var value2 = input2.get(key);
+            Map<String, Object> map = new HashMap<>();
+            if (!input1.containsKey(key)) {
+                // was added
+                map.put("key",key);
+                map.put("type","added");
+                map.put("value",value2);
+                answer.add(map);
+            } else if (!input2.containsKey(key)) {
+                // was removed
+                map.put("key",key);
+                map.put("type","deleted");
+                map.put("value",value1);
+                answer.add(map);
+            } else if (!Objects.deepEquals(value1, value2)) {
+                // was updated
+                map.put("key",key);
+                map.put("type","changed");
+                map.put("value1",value1);
+                map.put("value2",value2);
+                answer.add(map);
+            } else {
+                // was unchanged
+                map.put("key",key);
+                map.put("type","unchanged");
+                map.put("value",value1);
+                answer.add(map);
+            }
+        }
+
+        return  objectMapper.writeValueAsString(answer);
+
+    }
 
     public static String typeCheck(Object input) {
         if (input instanceof String) {
