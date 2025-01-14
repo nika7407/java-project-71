761664a9b11a599f23bb728fc785c8e1b986b37f
nika7407 <nikala7407@gmail.com>
2025-01-14 21:36:18 +0400

    #1 fixes

--- app/src/main/java/hexlet/code/Formatters/Json.java
+++ app/src/main/java/hexlet/code/Formatters/Json.java
@@ -3,8 +3,6 @@ package hexlet.code.Formatters;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
