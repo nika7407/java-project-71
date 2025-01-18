060ba82633251d278d76eac045597c2466a85d3d
nika7407 <nikala7407@gmail.com>
2025-01-18 22:12:48 +0400

    #2 fixes

--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -99,14 +99,7 @@ public final class AppTest {
     @Test
     public void testGenerateDiff() {
         String actual = stylish(diffListWithKeys);
-        String expected = "{\n"
-                + "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n"
-                + "}";
+        String expected = generetarorResult;
         assertEquals(expected, actual);
     }
 
