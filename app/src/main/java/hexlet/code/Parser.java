35eed7cbcead4f55e4d24d391e87adade72095e6
nika7407 <nikala7407@gmail.com>
2025-01-18 22:06:25 +0400

    #2 fixes

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -6,19 +6,20 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+
 import static hexlet.code.Differ.fileFormat;
 
 public class Parser {
 
-public static Map<String, Object> parse(String path) throws IOException {
-    String fileType = fileFormat(path);
-    if (fileType.equals("json")) {
-        return getDataJson(path);
-    } else if (fileType.equals("yaml") || fileType.equals("yml")) {
-        return  getDataYaml(path);
+    public static Map<String, Object> parse(String path) throws IOException {
+        String fileType = fileFormat(path);
+        if (fileType.equals("json")) {
+            return getDataJson(path);
+        } else if (fileType.equals("yaml") || fileType.equals("yml")) {
+            return getDataYaml(path);
+        }
+        return null;
     }
-    return null;
-}
 
     public static Map<String, Object> getDataJson(String filePath) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -29,8 +30,7 @@ public static Map<String, Object> parse(String path) throws IOException {
         ObjectMapper mapper = new YAMLMapper();
         return mapper.readValue(new File(filePath), Map.class);
     }
-
-
 }
+
 
 
